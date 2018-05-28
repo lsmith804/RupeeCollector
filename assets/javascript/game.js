a11ce15eac615851633e5ca28eee5afb8781c185
Lori Argentum <lori.smith@richmond.edu>
2018-05-28 01:29:24 -0400

    JQUERY homework again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,44 +15,66 @@ var wins = 0;
 var losses = 0;
 var winsLosses;
 
-var totalScore = 0;
 
 //Begin a new game
 $(document).ready(function() {
-console.log(randomNumber);
-$("#totalScore").append(totalScore);
-$("#randomNumber").append(randomNumber);
+    var totalScore = 0;
 
-    //Generate random values for each rupee
-    $("#greenButton").on("click", function () {
-        console.log(green);
-        //add this to totalScore
-        var newtotal1 = totalScore + green;
-        console.log(newtotal1);
-    });
+    $("#totalScore").append(totalScore);
+    $("#randomNumber").append(randomNumber);
 
-    $("#pinkButton").on("click", function () {
-        console.log(pink);
-    });
+    if (totalScore < randomNumber) { 
 
-    $("#orangeButton").on("click", function () {
-        console.log(orange);
-    });
-
-    $("#purpleButton").on("click", function () {
-        console.log(purple);
-    });
+        //Generate random values for each rupee & add to total
+        $("#greenButton").on("click", function () {
+            console.log(green);
+            totalScore = totalScore + green;
+            $("#totalScore").html(totalScore);
+            //if (totalScore > randomNumber) {
+                //console.log("STOP!");
 
 
-    //Add values of the rupees together
-    $(".button").on("click", function() {
+    
+            }
+        });
 
-    });
+        $("#pinkButton").on("click", function () {
+            console.log(pink);
+            totalScore = totalScore + pink;
+            $("#totalScore").html(totalScore);
+        });
+
+        $("#orangeButton").on("click", function () {
+            console.log(orange);
+            totalScore = totalScore + orange;
+            $("#totalScore").html(totalScore);
+
+        });
+
+        $("#purpleButton").on("click", function () {
+            totalScore = totalScore + purple;
+            $("#totalScore").html(totalScore);
+            console.log(purple);
+        });
+
+
+        //Add values of the rupees together
+        $("button").on("click", function() {
+            console.log("Your new total is "+ totalScore + ", so keep adding");
+
+        });
+
+        if (totalScore > randomNumber) {
+            $("button").on("click", function() {
+            console.log("STOP!");
+            });
+
+        }
+    } 
+
 
     //If statement needed to reset game if randomNumber is exceeded or met
-    if (totalScore < randomNumber) {
-        console.log("keep adding");
-    }
+
 
 });
 
