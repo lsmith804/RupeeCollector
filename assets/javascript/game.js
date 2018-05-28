36139e1bae52aac22882f357591b2545956f1819
Lori Argentum <lori.smith@richmond.edu>
2018-05-28 02:59:18 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
-// will be given a random number at the start of the game. There are four crystals below. By clicking on a crystal, you will add a specific amount of points to your score. You win the game by matching your total score to a random number. You lose the game if your total score goes above the random number.The value of each crystal is hidden from you until you click on it. Each time the game starts, the game will change the values of each crystal.
-
-
 // Store a random value between 19 and 120
 var randomNumber = [Math.floor(Math.random() * 121) + 19];
+console.log("the new random number is" + randomNumber);
 
 //Store random value between 1 and 12 for each crystal
 var green = Math.floor(Math.random() * 13) + 1;
@@ -10,29 +8,33 @@ var pink = Math.floor(Math.random() * 13) + 1;
 var orange = Math.floor(Math.random() * 13) + 1;
 var purple = Math.floor(Math.random() * 13) + 1;
 
-//Tally wins, losses, and guesses so far.
+//Wins, losses, and guesses so far.
 var wins = 0;
 var losses = 0;
 var winsLosses;
-var totalScore;
+var totalScore = 0;
 
 
 //Begin a new game
 $(document).ready(function() {
-    newGame();
-    function newGame() {
+
     totalScore = 0;
+    losses = 0;
 
     $("#totalScore").append(totalScore);
     $("#randomNumber").append(randomNumber);
+    $("#winCount").append(wins);
+    $("#lossCount").append(losses);
     
     if (totalScore === randomNumber) {
+        wins++;
         alert("You win!");
     }
     
     if (totalScore > randomNumber) {
+        losses++;
         alert("LOSER");
-        newgame();
+        newGame();
     }
 
 
@@ -44,9 +46,6 @@ $(document).ready(function() {
             console.log(green);
             totalScore = totalScore + green;
             $("#totalScore").html(totalScore);
-            if (totalScore === randomNumber) {
-                alert("You win!");
-            }
         });
 
         $("#pinkButton").on("click", function () {
@@ -87,6 +86,6 @@ $(document).ready(function() {
     } 
 
     //If statement needed to reset game if randomNumber is exceeded or met
-}
+
 
 });
