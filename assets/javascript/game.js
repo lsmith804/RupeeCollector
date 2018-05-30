95d5f8c7f55d1fd0f3bd766b455714b8597944ab
Lori Argentum <lori.smith@richmond.edu>
2018-05-29 23:58:03 -0400

    more updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,3 @@
-// Store a random value between 19 and 120
-var randomNumber = [Math.floor(Math.random() * 121) + 19];
-console.log("the new random number is" + randomNumber);
-
 //Store random value between 1 and 12 for each crystal
 var green = Math.floor(Math.random() * 13) + 1;
 var pink = Math.floor(Math.random() * 13) + 1;
@@ -14,31 +10,33 @@ var losses = 0;
 var winsLosses;
 var totalScore = 0;
 
+function loseOne() {
+    losses++;
+    $("#lossCount").html(losses);
+    alert("Sorry. Try again!");
+}
+
+function winOne() {
+    wins++;
+    $("#winCount").html(wins);
+    alert("Yay! You won!");
+    
+}
+
+function newGame() {
+    // Store a random value between 19 and 120
+    var randomNumber = [Math.floor(Math.random() * 121) + 19];
+    console.log("the new random number is" + randomNumber);
+    $("#randomNumber").text(randomNumber);
+
+
 
 //Begin a new game
 $(document).ready(function() {
-
-    totalScore = 0;
-    losses = 0;
-
     $("#totalScore").append(totalScore);
-    $("#randomNumber").append(randomNumber);
     $("#winCount").append(wins);
     $("#lossCount").append(losses);
     
-    if (totalScore === randomNumber) {
-        wins++;
-        alert("You win!");
-    }
-    
-    if (totalScore > randomNumber) {
-        losses++;
-        alert("LOSER");
-        newGame();
-    }
-
-
-
     if (totalScore < randomNumber) { 
 
         //Generate random values for each rupee & add to total
@@ -46,12 +44,14 @@ $(document).ready(function() {
             console.log(green);
             totalScore = totalScore + green;
             $("#totalScore").html(totalScore);
+
         });
 
         $("#pinkButton").on("click", function () {
             console.log(pink);
             totalScore = totalScore + pink;
             $("#totalScore").html(totalScore);
+
         });
 
         $("#orangeButton").on("click", function () {
@@ -69,15 +69,16 @@ $(document).ready(function() {
 
 
         //Check for win/loss
-
         $("button").on("click", function() {
             if (totalScore === randomNumber) {
                 alert("You win!");
+                winOne();
         }
 
             if(totalScore > randomNumber) {
-                alert("You lose!");
-
+                loseOne();
+                newGame();
+                alert("You lose and newGame Function called");
             }
         });
 
@@ -85,7 +86,7 @@ $(document).ready(function() {
         
     } 
 
-    //If statement needed to reset game if randomNumber is exceeded or met
 
 
-});
+})}
+newGame();
